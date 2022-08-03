1c2c95f2803b177d03a893040bdba69434300899
Paulo Sergio <pauloserg861@gmail.com>
2022-08-03 15:13:39 -0300

    adicionando estrutura conectada ao banco de dados

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document } from 'mongoose'
+
+export type UserDocument = User & Document
+
+@Schema()
+export class User {
+    @Prop()
+    id?: string
+
+    @Prop()
+    name: string
+
+    @Prop()
+    email: string
+
+    @Prop()
+    password: string
+
+    @Prop()
+    rules: string
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
