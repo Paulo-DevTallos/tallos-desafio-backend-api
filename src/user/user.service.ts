ae6c7ae9106a146a84360bcf24f913b53c248159
Paulo Sergio <pauloserg861@gmail.com>
2022-08-03 16:17:00 -0300

    criando rota de cadastro de usuario]

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(@InjectModel(User.name) private readonly userModel: Model<UserDocument>) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    const createNewUser = new this.userModel(createUserDto)
+
+    return createNewUser.save()
   }
 
   findAll() {
